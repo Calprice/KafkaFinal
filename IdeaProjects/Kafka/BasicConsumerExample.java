9f87ef8f2211e7c9929116292f605763cfe16256
Calvin Price <calprice@cisco.com>
2018-06-13 15:59:47 -0700

    Kafka files

---
+++ IdeaProjects/Kafka/BasicConsumerExample.java
@@ -0,0 +1,71 @@
+//Implemented by Simon
+
+import org.apache.kafka.clients.CommonClientConfigs;
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.consumer.ConsumerRebalanceListener;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.apache.kafka.common.TopicPartition;
+import org.apache.kafka.common.config.SslConfigs;
+
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Properties;
+
+public class BasicConsumerExample {
+    public static void main(String[] args) {
+
+        Properties props = new Properties();
+        props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG,
+                Arrays.asList(
+                        "mplkfk-stg1-01:9093",
+                        "mplkfk-stg1-02:9093",
+                        "mplkfk-stg2-01:9093",
+                        "mplkfk-stg2-02:9093"
+                ));
+
+        //configure the following three settings for SSL Encryption
+        //Is that the correct directory for the truststore
+        props.put(CommonClientConfigs.SECURITY_PROTOCOL_CONFIG, "SSL");
+        props.put(SslConfigs.SSL_TRUSTSTORE_LOCATION_CONFIG, "src/main/resources/kafka.client.truststore.jks");
+        props.put(SslConfigs.SSL_TRUSTSTORE_PASSWORD_CONFIG, "cisco123");
+
+        //configure the following three settings for SSL Authentication
+        //Is that the correct directory for the keystore?
+        props.put(SslConfigs.SSL_KEYSTORE_LOCATION_CONFIG, "src/main/resources/kafka.client.keystore.jks");
+        props.put(SslConfigs.SSL_KEYSTORE_PASSWORD_CONFIG, "cisco123");
+        props.put(SslConfigs.SSL_KEY_PASSWORD_CONFIG, "cisco123");
+
+        props.put(ConsumerConfig.GROUP_ID_CONFIG, "dce-smb-kafka");
+        props.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
+        props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, "org.apache.kafka.common.serialization.StringDeserializer");
+        props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, "org.apache.kafka.common.serialization.StringDeserializer");
+        KafkaConsumer<byte[], byte[]> consumer = new KafkaConsumer<>(props);
+        TestConsumerRebalanceListener rebalanceListener = new TestConsumerRebalanceListener();
+        consumer.subscribe(Collections.singletonList("mplOrderData"), rebalanceListener);
+
+        while (true) {
+            ConsumerRecords<byte[], byte[]> records = consumer.poll(1000);
+            for (ConsumerRecord<byte[], byte[]> record : records) {
+                System.out.printf("Received Message topic =%s, partition =%s, offset = %d, key = %s, value = %s\n", record.topic(), record.partition(), record.offset(), record.key(), record.value());
+            }
+
+            consumer.commitSync();
+        }
+
+    }
+
+    private static class TestConsumerRebalanceListener implements ConsumerRebalanceListener {
+        @Override
+        public void onPartitionsRevoked(Collection<TopicPartition> partitions) {
+            System.out.println("Called onPartitionsRevoked with partitions:" + partitions);
+        }
+
+        @Override
+        public void onPartitionsAssigned(Collection<TopicPartition> partitions) {
+            System.out.println("Called onPartitionsAssigned with partitions:" + partitions);
+        }
+    }
+}
